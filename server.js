cbac9195484cc363ef5f67634f3a91d77023a6ec
turbokungfu <slashrandazzo@gmail.com>
2022-07-27 15:38:27 -0700

    tried ejs, but need more info

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express= require('express');
 const mongoose= require('mongoose');
-
+const MongoClient = require('mongodb').MongoClient
 const app=express();
 
 const port= 3002;
 const url= "mongodb+srv://mike:letsdoit@cluster0.iuc2b.mongodb.net/?retryWrites=true&w=majority";
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
 
 mongoose.connect(url,{useNewUrlParser: true});
 const con= mongoose.connection;
@@ -21,8 +27,10 @@ try{
 const studentrouter= require("./routes/students");
 app.use('/students',studentrouter)
 
-
-
+app.set('view engine', 'ejs')
+app.use(express.static('public'))//view css html
+app.use(express.urlencoded({ extended: true })) //parses to strings
+app.use(express.json())//parse .json - middleware
 
 app.listen(port, () =>{
     console.log(`server started on port ${port}`);
